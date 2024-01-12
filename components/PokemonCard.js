b5fead5f8c3dd62df521e58eeab854a7792d9bbb
Gowtham Behara <gothsiddu@gmail.com>
2024-01-12 10:52:34 -0500

    Add styles for type, moves and weaknesses

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,5 +1,21 @@
 import { View, Text, StyleSheet, Platform, Image } from "react-native";
 
+const getTypeDetails = (type) => {
+    switch (type.toLowerCase()) {
+        case "electric":
+            return { borderColor: "#FFD700", emoji: "⚡️" };
+        case "water":
+            return { borderColor: "#6493EA", emoji: "💧" };
+        case "fire":
+            return { borderColor: "#FF5733", emoji: "🔥" };
+        case "grass":
+            return { borderColor: "#66CC66", emoji: "🌿" };
+        default:
+            return { borderColor: "#A0A0A0", emoji: "❓" };
+    }
+};
+
+
 const PokemonCard = ({
     name,
     image,
@@ -8,6 +24,9 @@ const PokemonCard = ({
     moves,
     weaknesses
 }) => {
+
+    const { borderColor, emoji } = getTypeDetails(type)
+
     return (
         <View style={styles.card}>
             <View style={styles.nameContainer}>
@@ -22,16 +41,19 @@ const PokemonCard = ({
                 resizeMode="contain"
             />
 
-            <View>
-                <Text>{type}</Text>
+            <View style={styles.typeContainer}>
+                <View style={[styles.badge, { borderColor }]}>
+                    <Text style={styles.typeEmoji}>{emoji}</Text>
+                    <Text style={styles.typeText}>{type}</Text>
+                </View>
             </View>
 
-            <View>
-                <Text>Moves: {moves.join(", ")}</Text>
+            <View style={styles.movesContainer}>
+                <Text style={styles.movesText}>Moves: {moves.join(", ")}</Text>
             </View>
 
-            <View>
-                <Text>Weaknesses: {weaknesses.join(", ")}</Text>
+            <View style={styles.weaknessContainer}>
+                <Text style={styles.weaknessText}>Weaknesses: {weaknesses.join(", ")}</Text>
             </View>
         </View>
     )
@@ -73,6 +95,42 @@ const styles = StyleSheet.create({
         width: "100%",
         height: 200,
         marginBottom: 16,
+    },
+    typeContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 40,
+    },
+    badge: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 6,
+        paddingHorizontal: 12,
+        borderRadius: 20,
+        borderWidth: 4,
+    },
+    typeEmoji: {
+        fontSize: 30,
+        marginRight: 12,
+    },
+    typeText: {
+        fontSize: 22,
+        fontWeight: "bold",
+    },
+    movesContainer: {
+        marginBottom: 12,
+    },
+    movesText: {
+        fontSize: 22,
+        fontWeight: "bold",
+    },
+    weaknessContainer: {
+        marginBottom: 8,
+    },
+    weaknessText: {
+        fontSize: 22,
+        fontWeight: "bold",
     },
 })
 
