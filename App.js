931394909beb1fb899c28dd572590e80c4a9e5f5
Gowtham Behara <gothsiddu@gmail.com>
2024-01-14 23:17:17 -0500

    dynamically fetch first generation pokemons image and name

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
-import { StyleSheet, SafeAreaView, Platform, ScrollView, FlatList, Text, View } from 'react-native';
-import PokemonCard from "./components/PokemonCard";
+import { StyleSheet, SafeAreaView, Platform, FlatList, Text } from 'react-native';
+import PokemonThumbnail from "./components/PokemonThumbnail";
 import { useEffect, useState } from 'react';
 
 const pokeApiPath = "https://pokeapi.co/api/v2/";
@@ -8,92 +8,53 @@ const pokeApiQuery = "pokemon?limit=151&offset=0";
 
 export default function App() {
 
-  const charmanderData = {
-    name: "Charmander",
-    image: require("./assets/charmander.png"),
-    type: "Fire",
-    hp: 39,
-    moves: ["Scratch", "Ember", "Growl", "Leer"],
-    weaknesses: ["Water", "Rock"],
-  };
-
-  const squirtleData = {
-    name: "Squirtle",
-    image: require("./assets/squirtle.png"), // Replace with the actual image path
-    type: "Water",
-    hp: 44,
-    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
-    weaknesses: ["Electric", "Grass"],
-  };
-
-  const bulbasaurData = {
-    name: "Bulbasaur",
-    image: require("./assets/bulbasaur.png"), // Replace with the actual image path
-    type: "Grass",
-    hp: 45,
-    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
-    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
-  };
-
-  const pikachuData = {
-    name: "Pikachu",
-    image: require("./assets/pikachu.png"), // Replace with the actual image path
-    type: "Electric",
-    hp: 35,
-    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
-    weaknesses: ["Ground"],
-  };
-
   const firstGenPokemonsPath = `${pokeApiPath}${pokeApiQuery}`;
-  const [firstGenPokemonsDetails, setFirstGenPokemonsDetails] = useState([])
+  const [firstGenPokemonsThumbnails, setFirstGenPokemonsThumbnails] = useState([])
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchFirstGenPokemons = async () => {
+      const firstGenPokemonsResponse = await fetch(firstGenPokemonsPath);
+      const firstGenPokemonsResponseBody = await firstGenPokemonsResponse.json();
       try {
-        const firstGenPokemonsResponse = await fetch(firstGenPokemonsPath);
-        const firstGenPokemonsResponseBody = await firstGenPokemonsResponse.json();
+        const promises = firstGenPokemonsResponseBody.results.map(async result => {
+          const pokemonUrl = result.url;
+          const pokemonResponse = await fetch(pokemonUrl);
 
-        // const promises = firstGenPokemonsResponseBody.results.map(async result => {
-        //   const response = await fetch(result.url);
+          if (!pokemonResponse.ok) {
+            throw new Error(`Failed to fetch data from ${pokemonUrl}`);
+          }
 
-        //   if (!response.ok) {
-        //     throw new Error(`Failed to fetch data from ${api}`);
-        //   }
+          const pokemonResponseBody = await pokemonResponse.json();
+          return {
+            "name": result.name.charAt(0).toUpperCase() + result.name.slice(1),
+            "image": pokemonResponseBody.sprites.front_default
+          };
+        });
 
-        //   const rawData = await response.json();
-        //   return rawData.name;
-        // });
-
-        // const results = await Promise.all(promises);
-        // const mergedData = results.reduce((acc, result) => acc.concat(result), []);
-
-        // setFirstGenPokemonsDetails(mergedData);
-
-        const firstGenPokemonsNames = firstGenPokemonsResponseBody.results.map(result => result.name);
-        setFirstGenPokemonsDetails(firstGenPokemonsNames);
+        const responses = await Promise.all(promises);
+        const mergedResponse = responses.reduce((acc, response) => acc.concat(response), []);
+        setFirstGenPokemonsThumbnails(mergedResponse);
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
         setLoading(false);
       }
     }
+
     fetchFirstGenPokemons();
   }, []);
 
-  const renderPokemon = ({ item }) => <View><Text>{item}</Text></View>;
+  const renderPokemon = ({ item }) => <PokemonThumbnail name={item.name} image={item.image} />;
 
-  if (loading) return <Text>Loading...</Text>;
+  if (loading) return <SafeAreaView style={styles.container}><Text>Loading....</Text></SafeAreaView>;
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList data={firstGenPokemonsDetails} renderItem={renderPokemon} />
-      {/* <ScrollView>
-        <PokemonCard {...charmanderData} />
-        <PokemonCard {...squirtleData} />
-        <PokemonCard {...bulbasaurData} />
-        <PokemonCard {...pikachuData} />
-      </ScrollView> */}
+      <FlatList
+        data={firstGenPokemonsThumbnails}
+        renderItem={renderPokemon}
+      />
     </SafeAreaView>
   );
 }
