b286bb99c7daf4e546a81771ccf39205d8d20504
Gowtham Behara <gothsiddu@gmail.com>
2024-01-15 13:35:00 -0500

    add navigation support for Home component

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button, SafeAreaView } from "react-native";
 
 
-const HomeScreen = () => (
-    <View style={[styles.container, styles.horizontal]}>
-        <Text>Welcome</Text>
-    </View>
+const HomeScreen = ({ navigation }) => (
+    <SafeAreaView>
+        <View style={[styles.container, styles.horizontal]}>
+            <View><Text>Welcome</Text></View>
+            <View><Button
+                title="FirstGenPokemons"
+                onPress={() => navigation.navigate('FirstGenPokemons')}
+            /></View>
+        </View>
+    </SafeAreaView>
+
 );
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
+        backgroundColor: '#f5f5f5',
+        paddingTop: Platform.OS == 'android' ? 20 : 50
     },
     horizontal: {
         flexDirection: 'row',
@@ -20,4 +29,5 @@ const styles = StyleSheet.create({
     },
 });
 
-export default LoadingScreen;
+
+export default HomeScreen;
