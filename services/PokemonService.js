17bafa31e35527b72e9a509886b002869bf448bd
Gowtham Behara <gothsiddu@gmail.com>
2024-01-15 20:15:12 -0500

    Add service to fetch pokemon by name

--- services/PokemonService.js
+++ services/PokemonService.js
@@ -13,4 +13,15 @@ const fetchPokemonById = async (pokemonId) => {
         });
 }
 
-export { fetchPokemonById };
+const fetchPokemonByName = async (pokemonName) => {
+    const pokeApiQuery = `pokemon/${pokemonName}`;
+
+    return api.get(pokeApiQuery)
+        .then((pokemonData) => ({ id: pokemonData.id, weight: pokemonData.weight, image: pokemonData.image }))
+        .catch((error) => {
+            console.log("Error in fetchPokemonById: ", error.message);
+            throw error;
+        });
+}
+
+export { fetchPokemonById, fetchPokemonByName };
