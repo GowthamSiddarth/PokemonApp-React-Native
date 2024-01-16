ff41bc928803478d7d3b92fc8182b33825c2e1ad
Gowtham Behara <gothsiddu@gmail.com>
2024-01-15 20:13:15 -0500

    update fetchPokemonById to use helper/api module

--- services/PokemonService.js
+++ services/PokemonService.js
@@ -1,18 +1,11 @@
 // src/services/PokemonService.js
 
-import { POKEMON_API_PATH } from "../assets/constants/API";
+import api from "../helpers/api";
 
 const fetchPokemonById = async (pokemonId) => {
     const pokeApiQuery = `pokemon/${pokemonId}`;
 
-    return fetch(`${POKEMON_API_PATH}${pokeApiQuery}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Failed to fetch Pokemon data");
-            }
-
-            return response.json();
-        })
+    return api.get(pokeApiQuery)
         .then((pokemonData) => ({ id: pokemonData.id, weight: pokemonData.weight, image: pokemonData.image }))
         .catch((error) => {
             console.log("Error in fetchPokemonById: ", error.message);
