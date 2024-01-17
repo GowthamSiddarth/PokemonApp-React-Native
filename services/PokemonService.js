817600d19721f5444ab7d1f7e7f0166bfd5b8966
Gowtham Behara <gothsiddu@gmail.com>
2024-01-17 12:05:23 -0500

    Skip redundant json() call on response

--- services/PokemonService.js
+++ services/PokemonService.js
@@ -8,7 +8,7 @@ const PokemonService = {
     fetchPokemonById: (pokemonId) => {
         const pokeApiQuery = `pokemon/${pokemonId}`;
 
-        return api.get(pokeApiQuery)
+        return api.getWithEndpoint(pokeApiQuery)
             .then((pokemonData) => {
                 const pokemon = createPokemon({
                     id: pokemonData.id,
@@ -27,7 +27,7 @@ const PokemonService = {
     fetchPokemonByName: (pokemonName) => {
         const pokeApiQuery = `pokemon/${pokemonName}`;
 
-        return api.get(pokeApiQuery)
+        return api.getWithEndpoint(pokeApiQuery)
             .then((pokemonData) => {
                 const pokemon = createPokemon({
                     id: pokemonData.id,
@@ -46,27 +46,30 @@ const PokemonService = {
     fetchFirstGenPokemons: () => {
         const pokeApiQuery = "pokemon?limit=151&offset=0";
 
-        return api.get(pokeApiQuery)
-            .then(firstGenPokemonsResponse => firstGenPokemonsResponse.json()
-                .then(firstGenPokemonsResponseBody => {
-                    const promises = firstGenPokemonsResponseBody.results.map(result =>
-                    (api.get(result.url).then((pokemonResponse) => {
-                        if (!pokemonResponse.ok) {
-                            throw new Error(`Failed to fetch data from ${pokemonUrl}`);
-                        }
-                        pokemonResponse.json()
-                            .then(pokemonResponseBody => ({
+        return api.getWithEndpoint(pokeApiQuery)
+            .then(firstGenPokemonsResponseBody => {
+                const promises = firstGenPokemonsResponseBody.results.map(result =>
+                    api.get(result.url)
+                        .then(pokemonResponseBody => {
+                            const pokemon = {
                                 "name": result.name.charAt(0).toUpperCase() + result.name.slice(1),
                                 "image": pokemonResponseBody.sprites.front_default
-                            }))
-                    })));
+                            };
+                            return pokemon;
+                        })
+                );
 
-                    return Promise.all(promises).then(responses => responses.reduce((acc, response) => acc.concat(response), []));
-                })
-                .catch((error) => {
-                    console.log("Error in PokemonService.fetchPokemonByName: ", error.message);
-                    throw error;
-                }));
+                return Promise.all(promises)
+                    .then(responses => responses.reduce((acc, response) => acc.concat(response), []))
+                    .catch(error => {
+                        console.log(error);
+                        throw error;
+                    });
+            })
+            .catch((error) => {
+                console.log("Error in PokemonService.fetchPokemonByName: ", error.message);
+                throw error;
+            });
     }
 }
 
