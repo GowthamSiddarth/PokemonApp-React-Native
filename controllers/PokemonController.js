62049c9213a28fb8126e359bfb42bd64418a6a50
Gowtham Behara <gothsiddu@gmail.com>
2024-01-17 12:04:12 -0500

    Correct the syntax for PokemonController as a js Object

--- controllers/PokemonController.js
+++ controllers/PokemonController.js
@@ -3,28 +3,28 @@
 import PokemonService from "../services/PokemonService";
 
 
-const PokemonController = () => {
+const PokemonController = {
 
     fetchPokemonById: (pokemonId) => PokemonService.fetchPokemonById(pokemonId)
         .then(pokemon => pokemon)
         .catch((error) => {
             console.log("Error in PokemonController.fetchPokemonById:", error);
             throw error;
-        });
+        }),
 
     fetchPokemonByName: (pokemonName) => PokemonService.fetchPokemonByName(pokemonName)
         .then(pokemon => pokemon)
         .catch((error) => {
             console.log("Error in PokemonController.fetchPokemonById:", error);
             throw error;
-        });
+        }),
 
     fetchFirstGenPokemons: () => PokemonService.fetchFirstGenPokemons()
         .then(pokemons => pokemons)
         .catch((error) => {
             console.log("Error in PokemonController.fetchFirstGenPokemons:", error);
             throw error;
-        });
+        }),
 }
 
 export default PokemonController;
