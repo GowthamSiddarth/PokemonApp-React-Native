c8f7f3490eee0d111d870a52ae2e544963eb99cf
Gowtham Behara <gothsiddu@gmail.com>
2024-01-17 12:04:49 -0500

    create another method for partial endpoint access

--- helpers/api.js
+++ helpers/api.js
@@ -3,8 +3,23 @@
 import { POKEMON_API_PATH } from "../assets/constants/API";
 
 const api = {
-    get: (endpoint) => {
-        return fetch(`${POKEMON_API_PATH}/${endpoint}`)
+    getWithEndpoint: (endpoint) => {
+        const uri = `${POKEMON_API_PATH}/${endpoint}`.trim();
+        return fetch(uri)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch data from ${endpoint}`);
+                }
+                return response.json();
+            })
+            .catch((error) => {
+                console.error(`Error in api.get(${endpoint}):`, error.message);
+                throw error;
+            });
+    },
+
+    get: (uri) => {
+        return fetch(`${uri}`)
             .then((response) => {
                 if (!response.ok) {
                     throw new Error(`Failed to fetch data from ${endpoint}`);
